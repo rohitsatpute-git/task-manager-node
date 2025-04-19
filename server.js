76faf5b7e20be2181ef3d690576ea81f8c558932
Rohit Satpute <rohitsatpute480@gmail.com>
2025-04-19 19:33:04 +0530

    added vercel json

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes);
 
+app.get('/test', (req, res) => res.send('<h1>working</h1>'))
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
